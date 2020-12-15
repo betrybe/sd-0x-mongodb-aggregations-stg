b54e16c0c997648906dbb65f80e0051d419ee440
nato-re <alvares.renato@hotmail.com>
2020-12-15 16:02:32 -0300

    fix lint

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,15 +1,15 @@
 db.trips.aggregate([
   {
     $match: {
-      birthYear: { $exists: 1, $ne: '' },
+      birthYear: { $exists: 1, $ne: "" },
     },
   },
-  { $project: { birthYear: { $toInt: '$birthYear' } } },
+  { $project: { birthYear: { $toInt: "$birthYear" } } },
   {
     $group: {
       _id: 1,
-      maiorAnoNascimento: { $max: '$birthYear' },
-      menorAnoNascimento: { $min: '$birthYear' },
+      maiorAnoNascimento: { $max: "$birthYear" },
+      menorAnoNascimento: { $min: "$birthYear" },
     },
   },
   { $project: { _id: 0 } },
