3fe2bf6d08c08dcdd13fc1c9394fa04fe8425f44
Thiago Mariotto <thiagom.mariotto@betrybe.com>
2021-06-14 18:31:07 -0300

    add req 1 a 14

---
+++ challenges/desafio9.js
@@ -0,0 +1,24 @@
+db.trips.aggregate([
+  {
+    $match: {
+      birthYear: { $exists: true, $ne: "" },
+    },
+  },
+  {
+    $addFields: { data: { $toInt: "$birthYear" } },
+  },
+  {
+    $group: {
+      _id: "",
+      maiorAnoNascimento: { $max: "$data" },
+      menorAnoNascimento: { $min: "$data" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      maiorAnoNascimento: "$maiorAnoNascimento",
+      menorAnoNascimento: "$menorAnoNascimento",
+    },
+  },
+]);
